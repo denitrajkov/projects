87335c427c0db0c1b49573c41d894a19963af67a
Deni Trajkov <denitrajkov@yahoo.com>
2023-09-25 21:31:05 +0200

    oganishop

---
+++ oganishop/src/components/Filters.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { ProductCategory, ProductSize } from "../types/homepage";
+import { useRouter } from "next/router";
+
+interface Props {
+  categories: ProductCategory[];
+  sizes: ProductSize[];
+}
+
+const Filters: React.FC<Props> = ({ categories, sizes }) => {
+  const router = useRouter();
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__item">
+        <h4>Department</h4>
+
+        {router.query.category && (
+          <div className="mb-3">
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => {
+                delete router.query.category;
+
+                router.push({
+                  href: "/shop",
+                  query: {
+                    ...router.query,
+                  },
+                });
+              }}
+            >
+              Clear filter <span className="ml-1">&#10005;</span>
+            </button>
+          </div>
+        )}
+        {categories.map((category) => {
+          return (
+            <div
+              key={category.slug}
+              className="sidebar__item__size"
+              onClick={() => {
+                router.push({
+                  href: "/shop",
+                  query: {
+                    ...router.query,
+                    category: category.slug,
+                  },
+                });
+              }}
+            >
+              <label
+                className={
+                  router.query.category === category.slug ? "active" : ""
+                }
+              >
+                {category.name}
+                <input type="radio" name="product-categories" />
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="sidebar__item">
+        <h4>Popular Size</h4>
+
+        {router.query.size && (
+          <div className="mb-3">
+            <button
+              className="btn btn-danger btn-sm"
+              onClick={() => {
+                delete router.query.size;
+
+                router.push({
+                  href: "/shop",
+                  query: {
+                    ...router.query,
+                  },
+                });
+              }}
+            >
+              Clear filter <span className="ml-1">&#10005;</span>
+            </button>
+          </div>
+        )}
+        {sizes.map((size) => {
+          return (
+            <div
+              key={size.slug}
+              className="sidebar__item__size"
+              onClick={() => {
+                router.push({
+                  href: "/shop",
+                  query: {
+                    ...router.query,
+                    size: size.slug,
+                  },
+                });
+              }}
+            >
+              <label
+                className={router.query.size === size.slug ? "active" : ""}
+              >
+                {size.name}
+                <input type="radio" name="product-sizes" />
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Filters;
---
+++ oganishop/src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import { ThemeProvider } from "styled-components";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import "../styles/bootstrap.min.css";
+import "../styles/elegant-icons.css";
+import "../styles/style.css";
+
+import type { AppProps } from "next/app";
+import { theme } from "../styles/theme";
+import { useState } from "react";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [globalTheme, setGlobalTheme] = useState<"light" | "dark">("light");
+
+  return (
+    <>
+      <ThemeProvider theme={theme[globalTheme]}>
+        <button
+          onClick={() => {
+            setGlobalTheme((prevState) =>
+              prevState === "light" ? "dark" : "light"
+            );
+          }}
+        >
+          Toggle theme
+        </button>
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
